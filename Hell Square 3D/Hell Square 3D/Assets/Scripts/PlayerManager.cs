effbe61d3a7706d5afbd078131fc2b5a32e35b9f
seokgiLee <plot_lee@naver.com>
2022-06-11 00:56:02 +0900

    220610
    점프, 회피, 아이템 추가

--- Hell Square 3D/Hell Square 3D/Assets/Scripts/PlayerManager.cs
+++ Hell Square 3D/Hell Square 3D/Assets/Scripts/PlayerManager.cs
@@ -5,26 +5,50 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public float speed;
+    public float jumpPower;
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
+    Rigidbody rigid;
     Animator anim;
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        GetInput(); // 입력
+        Move(); // 이동
+        Turn(); // 방향전환
+        Jump(); // 점프
+        Dodge(); // 회피
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+    void Move()
+    {
+        if(!isDodge) // 회피 중 방향전환 방지
+        {
+            moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        }
 
         if (wDown) // 걷기
         {
@@ -38,7 +62,48 @@ public class PlayerManager : MonoBehaviour
         // 걷기, 달리기 애니메이션
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump && moveVec == Vector3.zero && !isDodge)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && !isJump && moveVec != Vector3.zero && !isDodge)
+        {
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeEnd", 0.5f);
+        }
+    }
+
+    void DodgeEnd()
+    {
+        speed /= 2;
+        isDodge = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
